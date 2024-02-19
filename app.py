ece4b6aa4c433fb9d1f8a9493d5c2901d203e2e4
Stefan <stefanboehme12@gmail.com>
2024-02-19 13:44:51 +0100

    Delete funktion hinzugefügt, fehlermeldung bei zuvielen Einträgen hinzugefügt

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask, render_template, redirect, url_for, request, flash
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
 from wtforms import StringField
@@ -48,13 +48,34 @@ def start():
     global todo_liste_elemente
     #Abfrage der Datenbak für schon vorhandene td-Bereiche
     todo_bereiche_query = TodoBereich.query.all()
+    neue_todo_area_id = len(todo_bereiche_query)
+
+    icon_url = request.args.get('icon_url')
+
+    if icon_url is None:
+        icon_url = "Bilder/clipboard2.svg"
+
 
     #for todo_bereich in todo_bereiche_query:
        # print(f"odobereichquery: {todo_bereich.todo_bereich_name}")
 
 
     todo_bereich_id = request.args.get("todo_bereich_id")
+    print(f"TodoBereichid = {todo_bereich_id}")
     todo_bereich_name = request.args.get("todo_bereich_name")
+
+    todo_liste_für_id = TodoListeElement.query.filter_by(todo_bereich_id=todo_bereich_id).all()
+
+
+
+    #todo_liste_für_id = request.args.get("todo_liste_für_id")
+
+
+    #if request.args.get("todo_liste_für_id") is None:
+     #   todo_liste_für_id = []
+
+
+
 
     #global todo_liste_elemente
 
@@ -62,7 +83,6 @@ def start():
     #todo_liste_elemente = request.args.get("todo_liste_elemente")
 
 
-    print(f"TodobereichId: {todo_bereich_id}")
 
 
 
@@ -94,9 +114,17 @@ def start():
     #if add_todo_area_form.validate_on_submit():
     if request.method == "POST" :
 
+        #Checken wieviele Todobereiche es gibt. Bei 15 eine Fehlermeldung ausgeben
+        todo_bereiche_query = TodoBereich.query.all()
+        if len(todo_bereiche_query) >= 20:
+            flash("Sie können kein TodoBereich mehr hinzufügen", "error")
+            return redirect(url_for("start"))
+
+
+
         new_area = add_todo_area_form.new_area.data
 
-        print(f"New Area: {new_area}")
+       # print(f"New Area Id: {new_area.id}")
 
         todo_area = TodoBereich(
             todo_bereich_name=new_area
@@ -104,18 +132,30 @@ def start():
         db.session.add(todo_area)
         db.session.commit()
 
+        neuste_todo_area = TodoBereich.query.filter_by(todo_bereich_name=new_area).first()
+
+        #print(f"New Area TodoBereichId {neuste_todo_area.id}")
+        print(icon_url)
+
         #todo_bereiche.append(new_area)
         todo_bereiche_query = TodoBereich.query.all()
 
-        return redirect(url_for("start", todo_bereiche_query=todo_bereiche_query))
+        neue_todo_area_id = len(todo_bereiche_query)
+        neuste_todo_area = TodoBereich.query.filter_by(id=neue_todo_area_id).first()
+        print(f"Neuste Todoarea: {neuste_todo_area.todo_bereich_name}")
 
+        #print(neue_todo_area_id)
+        #neue_todo_area_id += 1
+
+        return redirect(url_for("start", todo_bereiche_query=todo_bereiche_query,
+                                neuste_todo_area = neuste_todo_area,neue_todo_area_id = neue_todo_area_id,
+                                icon_url=icon_url))
 
 
     return render_template("start.html", add_todo_task_form = add_todo_task_form,
                            todo_liste = todo_liste, add_todo_area_form = add_todo_area_form,
-                           todo_bereiche=todo_bereiche, todo_bereiche_query=todo_bereiche_query, todo_liste_elemente = todo_liste_elemente, todo_bereich_name = todo_bereich_name)
-
-
+                           todo_bereiche=todo_bereiche, todo_bereiche_query=todo_bereiche_query, todo_liste_elemente = todo_liste_elemente, todo_bereich_name = todo_bereich_name,
+                           icon_url = icon_url,todo_liste_für_id=todo_liste_für_id)
 
 
 
@@ -127,27 +167,21 @@ def todo_einsehen(todo_bereich_id):
 
     todo_bereich_name = request.args.get('bereich_name')
 
+    todo_bereich_id = todo_bereich_id
     #print((f"TodobereichId: {todo_bereich_id}"))
 
     todo_bereich = TodoBereich.query.get(todo_bereich_id)
 
     print(f" TodoBereichName = {todo_bereich.todo_bereich_name}")
 
-    todo_liste_für_id = TodoListeElement.query.filter_by(todo_bereich_id=todo_bereich.id).all()
-    todo_liste_elemente = [element.aufgabe for element in todo_liste_für_id]
-
-    #for element in todo_liste_für_id:
-        #print(f"TodoListeFürId: {element}")
+    #todo_liste_für_id = TodoListeElement.query.filter_by(todo_bereich_id=todo_bereich.id).all()
+    #todo_liste_elemente = [element.aufgabe for element in todo_liste_für_id]
 
 
 
-#todo_liste_laden = TodoListeElement.query.filter_by(todo_bereich_id = todo_bereich_id).all()
-#print(todo_liste_laden)
-
-    for element in todo_liste_für_id:
-        print(element)
-
-    return redirect(url_for("start", todo_bereich_id=todo_bereich.id, todo_bereich_name = todo_bereich.todo_bereich_name))
+    return redirect(url_for("start", todo_bereich_id=todo_bereich_id,
+                            todo_bereich_name = todo_bereich.todo_bereich_name,
+                            todo_liste_elemente = todo_liste_elemente))
 
     #return render_template("todo_einsehen.html", todo_liste_elemente = todo_liste_elemente)
 
@@ -162,6 +196,10 @@ def todo_erledigt(todo_id):
 def dropdown_button():
     todo_bereiche_query = TodoBereich.query.all()
 
+    print(f"Länge Todobereiche{len(todo_bereiche_query)}")
+
+    letzter_bereich_id = len(todo_bereiche_query)
+
     #icon_url = "Bilder/stars.svg"
 
     icon_url = request.args.get("icon_url")
@@ -174,8 +212,30 @@ def dropdown_button():
 
     #if icon_url is N
 
-    return render_template("todo_einsehen.html", todo_bereiche = todo_bereiche_query, icon_url = icon_url)
+    return render_template("todo_einsehen.html", todo_bereiche = todo_bereiche_query, icon_url = icon_url, letzter_bereich_id = letzter_bereich_id)
 
+
+
+@app.route("/post-löschen/<int:bereich_id>")
+def todo_bereich_löschen(bereich_id):
+
+    zu_löschender_todo_bereich = TodoBereich.query.get(bereich_id)
+    print(f"Zu löschender Todobereich: {zu_löschender_todo_bereich}")
+    db.session.delete(zu_löschender_todo_bereich)
+    db.session.commit()
+
+    return redirect(url_for("start"))
+
+@app.route("/todo-löschen/<int:todo_id>")
+def todo_löschen(todo_id):
+
+    todo_bereich_id = request.args.get("todo_bereich_id")
+
+    zu_löschende_todo_aufgabe = TodoListeElement.query.get(todo_id)
+    db.session.delete(zu_löschende_todo_aufgabe)
+    db.session.commit()
+
+    return redirect(url_for("todo_einsehen", todo_bereich_id=todo_bereich_id))
 
 
 if __name__ == '__main__':
